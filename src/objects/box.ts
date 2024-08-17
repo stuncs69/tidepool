e38a12f015a232b1ee30ef4b47a0d9dc2f9fd441
stuncs69 <0346436@student.rocvantwente.nl>
2024-08-17 20:37:45 +0200

    Update box.ts

--- src/objects/box.ts
+++ src/objects/box.ts
@@ -12,67 +12,74 @@ export class Box implements TideObject {
     zIndex: number;
 
     constructor(x: number, y: number, width: number, height: number, color = 'reset', title = '', zIndex = 0) {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.color = color;
-      this.title = title;
-      this.contents = [];
-      this.zIndex = zIndex;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.title = title;
+        this.contents = [];
+        this.zIndex = zIndex;
     }
-  
+
     addContent(content: TideObject) {
-      this.contents.push(content);
+        this.contents.push(content);
     }
-  
+
     clear(screen: string[][]) {
-      for (let i = this.y; i < this.y + this.height; i++) {
-        for (let j = this.x; j < this.x + this.width; j++) {
-          screen[i][j] = ' ';
+        const maxY = Math.min(this.y + this.height, screen.length);
+        const maxX = Math.min(this.x + this.width, screen[0].length);
+
+        for (let i = this.y; i < maxY; i++) {
+            for (let j = this.x; j < maxX; j++) {
+                screen[i][j] = ' ';
+            }
         }
-      }
     }
-  
+
     draw(screen: string[][]) {
-      this.clear(screen);
-  
-      const colorCode = getColorCode(this.color);
-      const titleColorCode = getColorCode('magenta');
-  
-      for (let i = this.x + 1; i < this.x + this.width - 1; i++) {
-        screen[this.y][i] = `${colorCode}─\x1b[0m`;
-      }
-      for (let i = this.x + 1; i < this.x + this.width - 1; i++) {
-        screen[this.y + this.height - 1][i] = `${colorCode}─\x1b[0m`;
-      }
-      for (let i = this.y + 1; i < this.y + this.height - 1; i++) {
-        screen[i][this.x] = `${colorCode}│\x1b[0m`;
-        screen[i][this.x + this.width - 1] = `${colorCode}│\x1b[0m`;
-      }
-  
-      screen[this.y][this.x] = `${colorCode}┌\x1b[0m`;
-      screen[this.y][this.x + this.width - 1] = `${colorCode}┐\x1b[0m`;
-      screen[this.y + this.height - 1][this.x] = `${colorCode}└\x1b[0m`;
-      screen[this.y + this.height - 1][this.x + this.width - 1] = `${colorCode}┘\x1b[0m`;
-  
-      if (this.title) {
-        const titlePosition = Math.max(this.x + 2, Math.min(this.x + this.width - 2 - this.title.length, this.x + 2));
-        for (let i = 0; i < this.title.length; i++) {
-          if (titlePosition + i < screen[0].length && this.y < screen.length) {
-            screen[this.y][titlePosition + i] = `${titleColorCode}${this.title[i]}\x1b[0m`;
-          }
+        const maxY = Math.min(this.y + this.height, screen.length);
+        const maxX = Math.min(this.x + this.width, screen[0].length);
+
+        if (this.x >= screen[0].length || this.y >= screen.length || maxY <= this.y || maxX <= this.x) {
+            return;
         }
-      }
-  
-      for (const content of this.contents) {
-        content.draw(screen, this.x + 1, this.y + 1, this.width - 2);
-      }
+
+        this.clear(screen);
+
+        const colorCode = getColorCode(this.color);
+        const titleColorCode = getColorCode('magenta');
+
+        for (let i = this.x + 1; i < maxX - 1; i++) {
+            screen[this.y][i] = `${colorCode}─\x1b[0m`;
+        }
+        for (let i = this.x + 1; i < maxX - 1; i++) {
+            screen[maxY - 1][i] = `${colorCode}─\x1b[0m`;
+        }
+        for (let i = this.y + 1; i < maxY - 1; i++) {
+            screen[i][this.x] = `${colorCode}│\x1b[0m`;
+            screen[i][maxX - 1] = `${colorCode}│\x1b[0m`;
+        }
+
+        screen[this.y][this.x] = `${colorCode}┌\x1b[0m`;
+        screen[this.y][maxX - 1] = `${colorCode}┐\x1b[0m`;
+        screen[maxY - 1][this.x] = `${colorCode}└\x1b[0m`;
+        screen[maxY - 1][maxX - 1] = `${colorCode}┘\x1b[0m`;
+
+        if (this.title) {
+            const titlePosition = Math.max(this.x + 2, Math.min(maxX - 2 - this.title.length, this.x + 2));
+            for (let i = 0; i < this.title.length && titlePosition + i < maxX; i++) {
+                screen[this.y][titlePosition + i] = `${titleColorCode}${this.title[i]}\x1b[0m`;
+            }
+        }
+
+        for (const content of this.contents) {
+            content.draw(screen, this.x + 1, this.y + 1, maxX - this.x - 2);
+        }
     }
-  
+
     move(dx: number, dy: number) {
-      this.x += dx;
-      this.y += dy;
-    }
-  }
-  
+        this.x += dx;
+        this.y += dy;
+      }
+}
